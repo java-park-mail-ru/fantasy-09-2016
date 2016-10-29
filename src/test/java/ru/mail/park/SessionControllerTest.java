83f310637c1d5d4d55a293816d6a0623610cc9dd
Alsaril <yakovlev1996@gmail.com>
2016-10-29 12:31:16 +0300

    abstract test class

--- src/test/java/ru/mail/park/SessionControllerTest.java
+++ src/test/java/ru/mail/park/SessionControllerTest.java
@@ -1,41 +1,18 @@
 package ru.mail.park;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import ru.mail.park.utility.Utility;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
-@Transactional
-@ActiveProfiles("test")
-public class SessionControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    private void createUser() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/user/")
-                .header("content-type", "application/json")
-                .content(Utility.object2JSON(new Utility.RegistrationRequest("login", "password", "email"))))
-                .andExpect(status().isOk());
-    }
+public class SessionControllerTest extends AbstractTest {
 
     @Test
     public void testLogin() throws Exception {
-        createUser();
+        testCreateUser();
         mockMvc.perform(MockMvcRequestBuilders.post("/api/session/")
                 .header("content-type", "application/json")
                 .content(Utility.object2JSON(new Utility.LoginRequest("", ""))))
@@ -57,7 +34,7 @@ public class SessionControllerTest {
 
     @Test
     public void testLogout() throws Exception {
-        createUser();
+        testCreateUser();
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/session/")
                 .header("content-type", "application/json"))
                 .andExpect(status().isBadRequest());
@@ -68,8 +45,8 @@ public class SessionControllerTest {
     }
 
     @Test
-    public void testGet() throws Exception {
-        createUser();
+    public void testGetUser() throws Exception {
+        testCreateUser();
         mockMvc.perform(MockMvcRequestBuilders.get("/api/session/")
                 .sessionAttr("login", "login"))
                 .andExpect(status().isOk())
--- src/test/java/ru/mail/park/UserControllerTest.java
+++ src/test/java/ru/mail/park/UserControllerTest.java
@@ -1,61 +1,29 @@
 package ru.mail.park;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import ru.mail.park.utility.Utility;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.jdbc.JdbcTestUtils.countRowsInTable;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc(print = MockMvcPrint.NONE)
-@Transactional
-@ActiveProfiles("test")
-public class UserControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private JdbcTemplate template;
+public class UserControllerTest extends AbstractTest {
 
     @Test
     public void testCreate() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/user/")
-                .header("content-type", "application/json")
-                .content(Utility.object2JSON(new Utility.RegistrationRequest("login", "password", "email"))))
-                .andExpect(status().isOk());
-        assertEquals(1, countRowsInTable(template, "user"));
+        testCreateUser();
     }
 
     @Test
     public void testGet() throws Exception {
-        testCreate();
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/login")
-                .sessionAttr("login", "login"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isMap())
-                .andExpect(jsonPath("$.login").value("login"))
-                .andExpect(jsonPath("$.email").value("email"));
+        testCreateUser();
+        testGetUser("email");
     }
 
     @Test
     public void testListAll() throws Exception {
-        testCreate();
+        testCreateUser();
         mockMvc.perform(MockMvcRequestBuilders.get("/api/user/")
                 .sessionAttr("login", "login"))
                 .andExpect(status().isOk())
@@ -69,17 +37,12 @@ public class UserControllerTest {
 
     @Test
     public void testPut() throws Exception {
-        testCreate();
+        testCreateUser();
         mockMvc.perform(MockMvcRequestBuilders.put("/api/user/")
                 .sessionAttr("login", "login")
                 .header("content-type", "application/json")
                 .content(Utility.object2JSON(new Utility.ChangeUserRequest("new_email"))))
                 .andExpect(status().isOk());
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/login")
-                .sessionAttr("login", "login"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isMap())
-                .andExpect(jsonPath("$.login").value("login"))
-                .andExpect(jsonPath("$.email").value("new_email"));
+        testGetUser("new_email");
     }
 }
